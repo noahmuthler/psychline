543f0a5aa44563d7c4015d805b5624e45f5ae13d
Kade Carlson <carlsonkade3232@gmail.com>
2021-02-07 23:26:22 -0500

    made small changes

--- psychline/src/App.js
+++ psychline/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return ( 
+        <div className="App">
+            <img src="assets/bannerPic.jpg"></img>
+        </div>
+    );
 }
 
 export default App;
