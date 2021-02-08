c17357fd7db9fa93b45d2a43209ec33de6c51c7f
kd09763 <60417921+kd09763@users.noreply.github.com>
2021-02-08 16:10:29 -0500

    Update SignUp.js

--- psychline/src/SignUp.js
+++ psychline/src/SignUp.js
@@ -2,15 +2,35 @@ import react from "react"
 import reactDOM from "react-dom"
 
 class SignUp extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={};
 
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event){
+
+
+    }
 
     render() {
-
         return (
+            <form >
+                <h2>Account Details</h2>
+                <label>Email Address{"\n"}
+                    <input
+                        name="email"
+                        type="email"
+                        value= {this.state.value}
+                        onChange={this.handleChange}
 
 
-
-
-        )
+                    
+                    
+                    />
+                </label>
+            </form>
+        );
     }
 }
