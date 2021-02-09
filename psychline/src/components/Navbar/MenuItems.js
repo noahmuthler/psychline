bf92957fd0d3463448eba9184498b892f7c9c927
kd09763 <60417921+kd09763@users.noreply.github.com>
2021-02-08 20:24:16 -0500

    Create MenuItems.js

---
+++ psychline/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+        title:'Home',
+        url:/* Url Later Down The Road */ '#',
+        cName:'nav-links-home'
+    },
+    {
+        title:'Login',
+        url:/* Url Later Down The Road */ '#',
+        cName:'nav-links-login'
+    },
+    {
+        title:'Register',
+        url:/* Url Later Down The Road */ '#',
+        cName:'nav-links-register'
+    },
+    {
+        title:'Account',
+        url:/* Url Later Down The Road */ '#',
+        cName:'nav-links-account'
+    },
+    {
+        title:'About Us',
+        url:/* Url Later Down The Road */ '#',
+        cName:'nav-links-us'
+    },
+]
