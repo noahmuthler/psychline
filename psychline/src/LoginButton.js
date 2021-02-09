e13fac72915da26e80b32297694f829d53895d98
Noah Muthler <noahmuthler@Noahs-MacBook-Air.local>
2021-02-09 15:52:42 -0500

    changed loginbutton.js

--- psychline/src/LoginButton.js
+++ psychline/src/LoginButton.js
@@ -1,7 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CloseIcon from '@material-ui/icons/Close';
 
-class LoginButton extends Component {
-    render() {
-        return <div>Login button</div>
-    }
+import "./LoginButton.css";
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        login
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">
+            Login
+            <CloseIcon onClick={handleClose} className="closeButton" />
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="email address"
+            type="email"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="password"
+            type="password"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+            <div className="link">
+                <Link href="" >
+                    make an account
+                </Link>
+                 ~ 
+                <Link href="">
+                    forget password
+                </Link>
+            </div>
+            
+          <Button onClick={handleClose} color="primary">
+                Login
+          </Button>
+          
+          
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
--- psychline/src/index.js
+++ psychline/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SignUp from "./SignUp";
+import LoginButton from "./LoginButton";
 
-ReactDOM.render(<SignUp />, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <SignUp />,
+        <LoginButton />
+    </div>, 
+    document.getElementById("root"));
