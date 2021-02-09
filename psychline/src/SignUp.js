f45e340d1fa7fa24c7ff178037bea318566c9b58
kd09763 <60417921+kd09763@users.noreply.github.com>
2021-02-08 19:38:51 -0500

    Update SignUp.js

--- psychline/src/SignUp.js
+++ psychline/src/SignUp.js
@@ -1,98 +1,118 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import './SignUp.css';
 
 class SignUp extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {
-
-        };
+        this.state = {};
 
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleInputChange(event) {
-
-
-    }
+    handleInputChange(event) { }
 
     render() {
         return (
-<<<<<<< HEAD
-            <body>
-                <div class="heading">
-                    <h1>Create Account</h1>
-                    <p>ReRegistering is really simple, just imput the required fields and your good to go! *Required</p>
-                </div>
+            <body >
+                <div class="heading" >
+                    <h1> Create Account </h1>
+                    <p> Registering is really simple, just imput the required fields and your good to go! * Required </p>
+                </div >
                 <form >
-                    <div class="grid-container-account">
-                        <div class="left-item">
-                            <h2>Account Details</h2>
-                            <label>Email Address<br />
+                    <div class="grid-container-account" >
+                        <div class="left-item" >
+                            <h2 > Account Details </h2>  <label > Email Address < br />
                                 <input
                                     type="email"
                                     value={this.state.email}
                                     onChange={this.handleChange}
-                                /><br />
+                                />  <br />
                             </label>
 
-                            <label>Username<br />
+                            <label > Username < br />
                                 <input
                                     type="text"
                                     value={this.state.username}
                                     onChange={this.handleChange}
                                 /><br />
                             </label>
-=======
-            <form >
-                <h2>Account Details</h2>
-                <label>Email Address<br />
-                    <input
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    /><br />
-                </label>
->>>>>>> parent of 36dfde2... Update SignUp.js
 
-                <label>Username<br />
-                    <input
-                        type="text"
-                        value={this.state.username}
-                        onChange={this.handleChange}
-                    /><br />
-                </label>
+                            <label > Password < br />
+                                <input
+                                    type="password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                /><br />
+                            </label>
 
-                <label>Password<br />
-                    <input
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    /><br />
-                </label>
+                            <label > Confirm Password < br />
+                                <input
+                                    type="password"
+                                    value={this.state.confirmPassword}
+                                    onChange={this.handleChange}
+                                /><br />
+                            </label> </div >
 
-                <label>Confirm Password<br />
-                    <input
-                        type="password"
-                        value={this.state.confirmPassword}
-                        onChange={this.handleChange}
-                    /><br />
-                </label>
+                        <div class="right-item" >
+                            <h2> Profile Details </h2>
+                            <label> Name < br />
+                                <input
+                                    type="text"
+                                    value={this.state.name}
+                                    onChange={this.handleChange}
+                                /><br />
+                            </label>
 
+                            <label> Date of Birth < br />
+                                <input
+                                    type="date"
+                                    value={this.state.dob}
+                                    onChange={this.handleChange}
+                                /><br />
+                            </label>
 
-                <h2>Profile Details</h2>
-                <label>Name:<br />
-                    <input
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.handleChange}
+                            <label > Gender < br />
+                                <select
+                                    value={this.state.gender}
+                                    onChange={this.handleChange} >
+                                    <option value="male" > Male </option>
+                                    <option value="female" > female </option>
+                                    <option value="other" > Other </option>
+                                </select > < br />
+                            </label>
 
-                    /><br />
+                            <label > Country < br />
+                                <select
+                                    value={this.state.country}
+                                    onChange={this.handleChange} >
+                                </select> < br />
+                            </label>
 
-                </label>
-            </form>
+                            <label > City < br />
+                                <input
+                                    type="text"
+                                    value={this.state.city}
+                                    onChange={this.handleChange}
+                                /><br />
+                            </label>
+
+                            <label > Terms of Service < br />
+
+                                <input
+                                    type="checkbox"
+                                    checked={this.state.tos}
+                                    onChange={this.handleChange}
+                                />
+                            </label >
+                        </div>
+                    </div>
+                </form >
+            </body>
         );
     }
 }
+
 
 export default SignUp;
