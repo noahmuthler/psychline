93313509abcc73b388a56e36d3e589b166dcc18d
noahmuthler <noahmuthler@gmail.com>
2021-02-09 19:34:10 -0500

    added aboutus.js

---
+++ psychline/src/components/Home/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import "./AboutUs.css";
+
+
+function AboutUs() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default AboutUs;
