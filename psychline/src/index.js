b5afab02778849cbe8b0b48a67aa9a6c125de661
kd09763 <60417921+kd09763@users.noreply.github.com>
2021-02-13 00:56:42 -0500

    Update index.js

--- psychline/src/index.js
+++ psychline/src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SignUp from "./SignUp";
 import App from "./App";
+import NewSignUp from "./NewSignUp";
 
 ReactDOM.render(<App />, document.getElementById("root"));
